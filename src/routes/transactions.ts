363b393ddf884b24b43f4fca91dcf91a99c7602a
Gabriel Barros <ugabrieldev@gmail.com>
2024-03-24 11:29:34 -0300

    feat: create transactions route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,14 +1,25 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { knexInstance } from "../db/database";
+import { z } from "zod";
 
-export async function transactionsRoutes(app: FastifyInstance){
-    app.get("/hello", async () => {
-        try {
-          const transactions = await knexInstance("transactions").select("*");
-          return transactions;
-        } catch (error) {
-          console.error("Error occurred while accessing database:", error);
-          throw new Error("Internal Server Error");
-        }
-      });
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post("/", async (request: FastifyRequest, reply: FastifyReply) => {
+    const createTransactionBosySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
+
+    const { title, amount, type } = createTransactionBosySchema.parse(
+      request.body
+    );
+
+    await knexInstance("transactions").insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === "credit" ? amount : amount * -1,
+    });
+
+    return reply.status(201).send({message: "Transaction created successfully"})
+  });
 }
