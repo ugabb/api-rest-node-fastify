2ebde86b0c760fec5e07afb13dce84e76ba20ae1
Gabriel Barros <ugabrieldev@gmail.com>
2024-04-08 16:51:02 -0300

    refact: Categorize the transaction routes.

--- src/test/transactions.spec.ts
+++ src/test/transactions.spec.ts
@@ -1,27 +1,29 @@
-import {test, beforeAll, afterAll} from "vitest"
-import request from "supertest"
-import {app} from "../app"
+import { test, beforeAll, afterAll, describe } from "vitest";
+import request from "supertest";
+import { app } from "../app";
 
-// espera pela aplicação está completa/pronta para então fazer os testes.
-// Pois o fastify basicamente tudo é assíncrono.
-beforeAll(async () => {
-    await app.ready()
-})
+// Categorizando os testes.
+describe("Transactions Routes", () => {
+  // espera pela aplicação está completa/pronta para então fazer os testes.
+  // Pois o fastify basicamente tudo é assíncrono.
+  beforeAll(async () => {
+    await app.ready();
+  });
 
-// fecha a aplicação após realizar os testes.
-afterAll(async () => {
-    await app.close()
-})
+  // fecha a aplicação após realizar os testes.
+  afterAll(async () => {
+    await app.close();
+  });
 
-
-// Teste de criação de transação
-test("POST /transactions | user can create a new transaction", async () => {
+  // Teste de criação de transação
+  test("POST /transactions | user can create a new transaction", async () => {
     await request(app.server)
-    .post("/transactions")
-    .send({
+      .post("/transactions")
+      .send({
         title: "New Transaction",
         amount: 5000,
-        type:"credit"
-    })
-    .expect(201)
-})
+        type: "credit",
+      })
+      .expect(201);
+  });
+});
