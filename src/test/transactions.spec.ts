525af7524fb544939e841e17f466cacfe5258cb1
Gabriel Barros <ugabrieldev@gmail.com>
2024-04-08 17:20:02 -0300

    feat: Add unit test for listing all transactions

--- src/test/transactions.spec.ts
+++ src/test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { test, beforeAll, afterAll, describe } from "vitest";
+import { test, beforeAll, afterAll, describe, it, expect } from "vitest";
 import request from "supertest";
 import { app } from "../app";
 
@@ -25,5 +25,39 @@ describe("Transactions Routes", () => {
         type: "credit",
       })
       .expect(201);
+  });
+
+  // É possível fazer teste com "it()" também.
+  // Teste de listagem de transações
+  it("should be able to list all transactions", async () => {
+    // cria uma requisição de criar uma transação.
+    const createTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "New Transaction",
+        amount: 5000,
+        type: "credit",
+      });
+
+    // pega os cookies, no caso o sessionId que é necessário
+    const cookies = createTransactionResponse.get("Set-Cookie");
+
+    // cria o request para listar as transações
+    const listTransactionsResponse = await request(app.server)
+      .get("/transactions") // rota
+      .set("Cookie", cookies || []) // set os cookies junto da requisição
+      .expect(200);
+
+    // espera que tenha um array contendo um objeto com os valores
+    // {
+    //     title: "New Transaction",
+    //     amount: 5000,
+    // }
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: "New Transaction",
+        amount: 5000,
+      }),
+    ]);
   });
 });
